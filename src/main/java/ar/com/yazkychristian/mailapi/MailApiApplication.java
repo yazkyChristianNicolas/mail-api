448348ef2d34d2d2b145dbd4d765a0efd3eef075
yazkyChristian <yazkychristian@gmail.com>
2020-11-19 23:15:07 -0300

    Move fremarkerTemaplesPath property value to main application class, and delete appConfig

--- src/main/java/ar/com/yazkychristian/mailapi/MailApiApplication.java
+++ src/main/java/ar/com/yazkychristian/mailapi/MailApiApplication.java
@@ -7,12 +7,12 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-import ar.com.yazkychristian.mailapi.config.AppConfig;
 import freemarker.template.Configuration;
 import freemarker.template.TemplateExceptionHandler;
 
@@ -22,8 +22,8 @@ public class MailApiApplication {
 	
 	Logger logger = LoggerFactory.getLogger(MailApiApplication.class);
 	
-	@Autowired
-	AppConfig appConfig;
+	@Value("${freemarker.templates.path}")
+	private String freeMarkerTemplatePath;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MailApiApplication.class, args);
@@ -32,9 +32,9 @@ public class MailApiApplication {
 	
 	@Bean
 	public Configuration freeMarkerConfig() throws IOException{
-		logger.info("Freemarker templates directory: " + appConfig.getFreeMarkerTemplatePath());
+		logger.info("Freemarker templates directory: " + freeMarkerTemplatePath);
 		Configuration cfg = (Configuration) new Configuration(freemarker.template.Configuration.VERSION_2_3_29);
-		cfg.setDirectoryForTemplateLoading( new File(appConfig.getFreeMarkerTemplatePath()));
+		cfg.setDirectoryForTemplateLoading( new File(freeMarkerTemplatePath));
 		cfg.setDefaultEncoding("UTF-8");
 		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
 		cfg.setLogTemplateExceptions(false);

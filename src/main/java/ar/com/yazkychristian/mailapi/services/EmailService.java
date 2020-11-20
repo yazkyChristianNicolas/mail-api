79d11d7dc138dfa90d6c46ee8ccd237e40312286
yazkyChristian <yazkychristian@gmail.com>
2020-11-20 08:43:41 -0300

    Add some exceptions handlers

--- src/main/java/ar/com/yazkychristian/mailapi/services/EmailService.java
+++ src/main/java/ar/com/yazkychristian/mailapi/services/EmailService.java
@@ -1,8 +1,6 @@
 package ar.com.yazkychristian.mailapi.services;
 
-import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +10,7 @@ import org.springframework.stereotype.Service;
 
 import ar.com.yazkychristian.mailapi.beans.SendMailRequest;
 import freemarker.template.Configuration;
-import freemarker.template.MalformedTemplateNameException;
 import freemarker.template.Template;
-import freemarker.template.TemplateNotFoundException;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 
@@ -29,30 +25,19 @@ public class EmailService {
 	@Autowired
 	private Configuration freeMarkerConfig;
 	
+	//Could throw TemplateNotFoundException, MalformedTemplateNameException, MessagingException
 	public void sendMail(SendMailRequest mailRequest) throws Exception{
 		MimeMessage message = sender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message);	
 		String htmlMessage = null;
-		try {
-			Template temp = freeMarkerConfig.getTemplate(mailRequest.getTemplate());
-			htmlMessage = FreeMarkerTemplateUtils.processTemplateIntoString(temp, mailRequest.getData());
-			
-			helper.setTo(mailRequest.getTo());
-			helper.setText(htmlMessage, true);
-			helper.setSubject(mailRequest.getSubject());
-			helper.setFrom(mailRequest.getFrom());
-			sender.send(message);
-			
-		} catch (TemplateNotFoundException e) {
-			logger.error("Template not found", e);
-			throw e;
-		} catch(MalformedTemplateNameException mte) {
-			logger.error("MalformedTemplateNameException", mte);
-			throw mte;
-		} catch (MessagingException me) {
-			logger.error("Hubo un error al enviar el mail: {}", me);
-			throw me;
-		}		
 		
+		Template temp = freeMarkerConfig.getTemplate(mailRequest.getTemplate());
+		htmlMessage = FreeMarkerTemplateUtils.processTemplateIntoString(temp, mailRequest.getData());
+			
+		helper.setTo(mailRequest.getTo());
+		helper.setText(htmlMessage, true);
+		helper.setSubject(mailRequest.getSubject());
+		helper.setFrom(mailRequest.getFrom());
+		sender.send(message);
 	}
 }
